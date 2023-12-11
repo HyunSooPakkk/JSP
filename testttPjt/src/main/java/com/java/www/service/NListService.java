8fcfc56b604d6d81e1003324eb3343131a6cc582
KOREAVC <KOREAVC@DESKTOP-AI48KSJ>
2023-12-11 19:59:53 +0900

    Test

---
+++ testttPjt/src/main/java/com/java/www/service/NListService.java
@@ -0,0 +1,26 @@
+package com.java.www.service;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.java.www.dao.BoardDao;
+import com.java.www.dto.BoardDto;
+
+public class NListService implements Service {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		//가져올 데이터가 있으면 request.getParameter();로 불러오면 됨
+		//Dao 접근
+		BoardDao bdao = new BoardDao();
+		ArrayList<BoardDto> list = bdao.SelectAll();
+		
+		//Request에 담기
+		request.setAttribute("list", list);
+		
+		
+	}
+
+}
